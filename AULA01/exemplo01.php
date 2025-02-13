09cfaffe638cb5469ea71abbb879d09b28b46614
Victor Gabriel Bessemer Santos <161629490+Victorbessemer2@users.noreply.github.com>
2025-02-13 18:22:20 -0300

    Add files via upload

---
+++ AULA01/exemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Oi 2f, sou o Victor</p>
+    <?php
+        $nome = "Victor";
+        echo "<p>Oi 2f, sou o Victor </p>";
+        echo "<p>Oi 2f, sou o $nome </p>";
+        echo "<p>Oi 2f, sou o ".$nome."</p>";
+    ?>
+</body>
+</html>
