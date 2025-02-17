e2adfbc8abf7ba47caa99b04123be386e97e7889
Victor Gabriel Bessemer Santos <161629490+Victorbessemer2@users.noreply.github.com>
2025-02-17 14:59:46 -0300

    Add files via upload

---
+++ AULA01/exemplo04.php
@@ -0,0 +1,21 @@
+<?php
+    define('victor',1.5);
+    $x = 1.5;
+    echo "o valor e "." ".victor;
+    echo "<br>";
+    echo "o valor e $x";
+    echo "<br>";
+    $nome = "victor";
+    $idade = 16;
+    $funcao = "aluno";
+
+    $frase = "Dados pessoais: ";
+    $frase .= $nome;
+    $frase .= "_";
+    $frase .= $idade;
+    $frase .= "_";
+    $frase .= $funcao;
+
+    echo $frase;
+
+    ?>
---
+++ AULA01/exemplo05.php
@@ -0,0 +1,15 @@
+<?php
+    $nome = "victor";
+    $idade = 16;
+    $funcao = "aluno";
+
+    $frase = "Dados pessoais: ";
+    $frase .= $nome;
+    $frase .= "_";
+    $frase .= $idade;
+    $frase .= "_";
+    $frase .= $funcao;
+
+    echo $frase;
+
+    ?>
