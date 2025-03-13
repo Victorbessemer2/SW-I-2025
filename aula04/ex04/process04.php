d7889a8b5b687e8a951a500eccd9b7e832fad907
Victor Gabriel Bessemer Santos <161629490+Victorbessemer2@users.noreply.github.com>
2025-03-13 10:04:06 -0300

    termino exercicio

---
+++ aula04/ex04/process04.php
@@ -0,0 +1,15 @@
+<?php
+
+$username = $_POST['username'];
+$user_email = $_POST['user-email'];
+$user_born = $_POST['user-data'];
+$user_card = $_POST['card_flags'];
+
+echo "Hello, $username<br>";
+echo "Your e-mail is : $user_email<br>";
+echo "You born in $user_born<br>";
+echo "Your card flag is : $user_card<br>"
+
+
+
+?>
